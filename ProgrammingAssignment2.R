0a2921d4036525179763a0041be1f20daaea88d1
sitianwar985 <sitianwar985@gmail.com>
2020-11-22 23:46:39 +0800

    Explaination for functions, by line added

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -1,3 +1,24 @@
+#### FUNCTION 1: make cache matrix - create "matrix" object that can cache its inverse
+##assume matrix is invertible
+# LINE 1: assign Null to inv
+# LINE 2: set value of matrix 
+          # <- : works on current level
+          # <<-: modify variables on parent level
+# LINE 3: get value of matrix 
+# LINE 4: set value of inverse - use <<-
+# LINE 5: get value of inverse
+# LINE 6: create a list 
+
+#### FUNCTION 2: cachesolve - computes inverse of "matrix" - 
+#if inverse has been calculated & matrix hasnt been changed- retrieve inverse from cache
+# LINE 1: returns a matrix of inverse of X,and assign to inv
+# LINE 2: check if inverse if already been calculated, 
+#can get inverse from cache, can skip computation, return inverse
+# otherwise compute inverse of matrix, by using Solve
+# LINE 3: solve using SOLVE
+# LINE 4: Set value of the inverse in the cache using setInverse function
+
+
 makeCacheMatrix <- function(x = matrix ()) {
         invrs <- NULL
         set <- function(y){
@@ -9,6 +30,7 @@ makeCacheMatrix <- function(x = matrix ()) {
         getInverse <- function() {invrs}
         list(set =set, get =get, setInverse = setInverse, getInverse=getInverse)
 }
+
 
 cacheSolve <- function(x, ...) {
         invrs <- x$getInverse()
